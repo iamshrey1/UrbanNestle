3bf6d82f0e2d42894c6f49a34d7c01a5dadf09d9
Shreyansh <shreaksss@gmail.com>
2023-11-25 17:47:29 +0530

    created a sign up ui and a middleware

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv'
+import authRouter from './routes/auth.route.js';
 import UserRouter from './routes/user.route.js'
 dotenv.config();
 
@@ -12,9 +13,22 @@ mongoose.connect(process.env.MONGO).then(()=>{
 
 
 const app = express();
+app.use(express.json());
 
 
+//all these are middlewares
 app.use('/api/user',UserRouter);
+app.use('/api/auth', authRouter);
+
+app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || 'Internal Server Error';
+    return res.status(statusCode).json({
+      success: false,
+      statusCode,
+      message,
+    });
+  });
 
 
 app.listen(3000,()=>{

66e1d02f49c4bd9478e38015223afc44128c3e7f
Shreyansh <shreaksss@gmail.com>
2023-11-26 23:57:23 +0530

    protection throgh private route and profile ui

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,7 @@ import SignUp from "./pages/SignUp"
 import Profile from "./pages/Profile"
 import About from "./pages/About"
 import Header from "./components/header"
+import PrivateRoute from './components/PrivateRoute';
 
 export default function App() {
   
@@ -14,7 +15,9 @@ export default function App() {
     <Route path = "/" element={<Home/>} />
     <Route path = "sign-in" element={<SignIn/>} />
     <Route path = "/sign-up" element={<SignUp/>} />
-    <Route path = "/profile" element={<Profile/>} />
+    <Route element={<PrivateRoute />}>
+          <Route path='/profile' element={<Profile />} />
+    </Route>
     <Route path = "/about" element={<About/>} />
   </Routes>
   </BrowserRouter>

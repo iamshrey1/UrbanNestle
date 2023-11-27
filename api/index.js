768863009269956d8e6402a1bca36997dc86b3e4
Shreyansh <shreaksss@gmail.com>
2023-11-27 09:52:16 +0530

    add signin,signout,update,listing api

--- api/index.js
+++ api/index.js
@@ -3,6 +3,8 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv'
 import authRouter from './routes/auth.route.js';
 import UserRouter from './routes/user.route.js'
+
+import cookieParser from 'cookie-parser';
 dotenv.config();
 
 mongoose.connect(process.env.MONGO).then(()=>{
@@ -14,6 +16,7 @@ mongoose.connect(process.env.MONGO).then(()=>{
 
 const app = express();
 app.use(express.json());
+app.use(cookieParser())
 
 
 //all these are middlewares
